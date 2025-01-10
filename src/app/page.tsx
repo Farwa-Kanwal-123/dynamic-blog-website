af54a4e4bd1f2cf21d15cabb3a05b4e8a8d996a6
Farwa-Kanwal-123 <wahabzahid06@gmail.com>
2025-01-10 17:57:02 +0500

    make cards dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,11 +83,11 @@ export default function Home() {
           <h2 className="text-3xl font-semibold mb-12 text-center text-blue-800">Featured Adventures</h2>
           <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {[
-              { title: "Bali Bliss", description: "Exploring the Island of Gods", img: "/bali-bliss.jpeg" },
-              { title: "Tokyo Nights", description: "Neon lights and sushi delights", img: "/tokyo-night.jpeg" },
-              { title: "Amazon Expedition", description: "Into the heart of the rainforest", img: "/amazone-apedation.jpeg" }
-            ].map((adventure, index) => (
-              <Card key={index} className="hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1">
+              {id:1, title: "Bali Bliss", description: "Exploring the Island of Gods", img: "/bali-bliss.jpeg" },
+              {id:2, title: "Tokyo Nights", description: "Neon lights and sushi delights", img: "/tokyo-night.jpeg" },
+              {id:3, title: "Amazon Expedition", description: "Into the heart of the rainforest", img: "/amazone-apedation.jpeg" }
+            ].map((adventure) => (
+              <Card key={adventure.id} className="hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1">
                 <CardHeader>
                   <CardTitle className="text-xl sm:text-2xl">{adventure.title}</CardTitle>
                   <CardDescription>{adventure.description}</CardDescription>
@@ -102,7 +102,7 @@ export default function Home() {
                   />
                 </CardContent>
                 <CardFooter>
-                  <Link href="/blog">
+                  <Link href={`/blog/${adventure.id}`}>
                     <Button variant="ghost" className="bg-blue-600 hover:bg-blue-800 text-white hover:text-white">Read More</Button>
                   </Link>
                 </CardFooter>
